e9d27a10718335de9f510985ae4cfafcdf86e687
Vinay328 <vinay.k2233@gmail.com>
2018-08-29 19:56:16 +0530

    Master - Establishing socket server

---
+++ index.js
@@ -0,0 +1,27 @@
+let express = require('express');
+let socket = require('socket.io');
+let path = require('path');
+
+//App setup
+let app = express();
+
+//create a server
+let server = app.listen(4000);
+
+//socket set up
+let io = socket(server); // takes a parameter to what server we wanna work with
+
+io.on('connection', function (socket) {
+
+    // 10 different clients // unique id - different worlds
+    console.log("Made a new socket connection" + socket.id);
+
+    socket.on('chat', function (data) {
+        socket.broadcast.emit('chat', data); // to all sockets except client
+    });
+
+});
+
+app.get('/', function (req, res, next) {
+    res.sendFile(path.join(__dirname + '/views/index.html'));
+});
